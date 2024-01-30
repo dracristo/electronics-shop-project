72615403bc2d212e6177c49181eeac8fdbab05bf
dracristo <coding.enclosure@gmail.com>
2024-01-30 20:28:25 +0500

    сделал дз #1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item, item1
+
+
+def test_item():
+    assert type(Item.all) is list
+    assert type(Item.pay_rate) is float
+    assert item1.calculate_total_price() == 20.0
+    assert item1.apply_discount() is None
