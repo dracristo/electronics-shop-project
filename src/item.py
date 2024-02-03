8ba4b5306a19a0bbce10b8223f47c34e837296ba
dracristo <coding.enclosure@gmail.com>
2024-02-03 16:54:59 +0500

    дз №2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,8 +8,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name, price, quantity) -> None:
         """
         Создание экземпляра класса item.
 
@@ -14,11 +16,31 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
 
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        if len(value) > 10:
+            self.__name = value[:10]
+        else:
+            self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls, file) -> None:
+        with open(file, "r", encoding='utf-8') as f:
+            csv_reader = csv.DictReader(f, delimiter=",")
+            for row in csv_reader:
+                Item.all.append(Item(row["name"], row["price"], row["quantity"]))
+
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
 
     def calculate_total_price(self) -> float:
         """
@@ -28,12 +50,8 @@ class Item:
         """
         return self.price * Item.pay_rate
 
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.calculate_total_price()
-
-
-item1 = Item("q", 20, 1)
